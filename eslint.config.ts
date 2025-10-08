0eb6a080918b9f270decbc552d16ce64a6a3d511
Damien Boisvert <damien@alphagame.dev>
2025-10-08 16:23:14 -0700

    stricter eslint rules

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,20 +1,62 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
+
 export default tseslint.config(
-  eslint.configs.recommended, // Core ESLint recommended rules
-  ...tseslint.configs.recommended, // TypeScript-specific recommended rules
-  {
-    files: ['**/*.ts', '**/*.tsx'], // Apply these settings to TypeScript files
-    rules: {
-      // Custom TypeScript-specific rules
-      '@typescript-eslint/explicit-function-return-type': 'off',
+    eslint.configs.recommended, // Core ESLint recommended rules
+    ...tseslint.configs.recommended, // TypeScript-specific recommended rules
+    {
+        files: ['**/*.ts', '**/*.tsx'], // Apply these settings to TypeScript files
+        rules: {
+            // Custom TypeScript-specific rules
+            '@typescript-eslint/explicit-function-return-type': 'off',
+            'arrow-spacing': ['warn', { before: true, after: true }],
+            'brace-style': ['error', 'stroustrup', { allowSingleLine: true }],
+            'comma-dangle': ['error', 'always-multiline'],
+            'comma-spacing': 'error',
+            'comma-style': 'error',
+            curly: ['error', 'multi-line', 'consistent'],
+            'dot-location': ['error', 'property'],
+            'handle-callback-err': 'off',
+            indent: ['error', 'tab'],
+            'keyword-spacing': 'error',
+            'max-nested-callbacks': ['error', { max: 4 }],
+            'max-statements-per-line': ['error', { max: 2 }],
+            'no-console': 'off',
+            'no-empty-function': 'error',
+            'no-floating-decimal': 'error',
+            'no-inline-comments': 'error',
+            'no-lonely-if': 'error',
+            'no-multi-spaces': 'error',
+            'no-multiple-empty-lines': ['error', { max: 2, maxEOF: 1, maxBOF: 0 }],
+            'no-shadow': ['error', { allow: ['err', 'resolve', 'reject'] }],
+            'no-trailing-spaces': ['error'],
+            'no-var': 'error',
+            'no-undef': 'off',
+            'object-curly-spacing': ['error', 'always'],
+            'prefer-const': 'error',
+            quotes: ['error', 'single'],
+            semi: ['error', 'always'],
+            'space-before-blocks': 'error',
+            'space-before-function-paren': [
+                'error',
+                {
+                    anonymous: 'never',
+                    named: 'never',
+                    asyncArrow: 'always',
+                },
+            ],
+            'space-in-parens': 'error',
+            'space-infix-ops': 'error',
+            'space-unary-ops': 'error',
+            'spaced-comment': 'error',
+            yoda: 'error',
+        },
     },
-  },
-  {
-    files: ['**/*.js', '**/*.jsx'], // Apply these settings to JavaScript files
-    rules: {
-      // Custom JavaScript-specific rules
-    },
-  }
+    {
+        files: ['**/*.js', '**/*.jsx'], // Apply these settings to JavaScript files
+        rules: {
+            // Custom JavaScript-specific rules
+        },
+    }
 );
