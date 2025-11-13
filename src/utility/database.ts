3f3ec5d45a4ecf5fa18cb9f6eefd2b4fa082b4fc
Damien Boisvert <damien@alphagame.dev>
2025-11-13 09:42:13 -0800

    cleanup: Add correct license header to database.ts [no ci]

--- src/utility/database.ts
+++ src/utility/database.ts
@@ -1,10 +1,20 @@
 // This file is a part of AlphaGameBot.
 // 
 //     AlphaGameBot - A Discord bot that's free and (hopefully) doesn't suck.
-//     Copyright (C) 2025  Damien Boisvert
+//     Copyright (C) 2025  Damien Boisvert (AlphaGameDeveloper)
 // 
-//     Licensed under the GNU GPL v3 or later.
-//     See <https://www.gnu.org/licenses/>.
+//     AlphaGameBot is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+// 
+//     AlphaGameBot is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+// 
+//     You should have received a copy of the GNU General Public License
+//     along with AlphaGameBot.  If not, see <https://www.gnu.org/licenses/>.
 
 import { PrismaClient } from "@prisma/client";
 import { AsyncLocalStorage } from "node:async_hooks";
